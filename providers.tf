1769412b81aee487308d464930ec4d8b39f33897
root <root@sumanth.wgiodsi1u4luba43c2rwt2zv4c.rx.internal.cloudapp.net>
2022-12-26 05:13:31 +0000

    provider file

--- providers.tf
+++ providers.tf
@@ -11,4 +11,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+ subscription_id = "855b3c2f-f781-4828-a396-d5749d577e21"
+  tenant_id       = "ca84daf0-3cd3-4718-bd17-bd6bdb5f1cf9"
+  client_id       = "a322d1f1-941b-4499-b3c1-ce1f38a2808f"
+  client_secret   =  "EGn8Q~bBfohTEDxoDRH-5cWdY3eE5D.zsRrxEawP"
 }
